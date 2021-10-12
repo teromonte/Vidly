d224982f27f42a993b1969a908e0fec5d60899dc
thiag <thiag@LAPTOP-5T7RCQ94>
2021-10-12 18:49:55 +0100

    video acaba aqui

---
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+
+        public Movie Movie { get; set; }
+
+        public List<Costumer> Custumers { get; set; }
+    }
+}
